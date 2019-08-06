f80dbde7891a36ed969e94d035135ab7fcdf8c1c
Sechere <jermains@computercastlesltd.co.ke>
2019-08-06 20:08:20 +0300

    [06/08/2019] Created a class for students

---
+++ student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentRankingApplication
+{
+    class student
+    {
+
+        public string name { get; set; }
+        public int maths { get; set; }
+        public int english { get; set; }
+        public int science { get; set; }
+        public int total { get; set; }
+        public int position { get; set; }
+
+        public student(string name, int maths, int english, int science)
+        {
+
+            this.name = name;
+            this.maths = maths;
+            this.english = english;
+            this.science = science;
+            total = maths + english + science;
+        }
+    }
+}
